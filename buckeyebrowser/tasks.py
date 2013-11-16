6d1e006fcf5e06b01ef9871525872feae8056da7
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2013-11-16 15:53:20 -0800

    Update

--- buckeyebrowser/tasks.py
+++ buckeyebrowser/tasks.py
@@ -62,9 +62,9 @@ def load_dialogs():
 
 @task()
 def load_database():
-    res = chord(load_segments.subtask(),
-                            load_categories.subtask(),
-                            load_speakers.subtask())(load_dialogs.s())
+    res = chord([load_segments.s(),
+                            load_categories.s(),
+                            load_speakers.s()])(load_dialogs.s())
     res.get()
 
 @task()
